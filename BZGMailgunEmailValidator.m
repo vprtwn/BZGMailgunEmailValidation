9fc5b461ea81c3e127c4f371e0ef8cf38c88b838
Ben Guo <benzguo@gmail.com>
2013-10-20 14:29:28 -0400

    Update BZGMailgunEmailValidator.m

--- BZGMailgunEmailValidator.m
+++ BZGMailgunEmailValidator.m
@@ -15,6 +15,7 @@
     if (validator) {
         validator.publicKey = publicKey;
         validator.operationQueue = operationQueue;
+        validator.performsFallbackValidation = YES;
     }
     return validator;
 }
@@ -24,38 +25,60 @@
                      failure:(void (^)(NSError *error))failure
 {
     NSURL *baseURL = [NSURL URLWithString:@"https://api.mailgun.net/v2/"];
+    NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+    operationQueue.maxConcurrentOperationCount = NSOperationQueueDefaultMaxConcurrentOperationCount;
+
     NSURL *url = [NSURL URLWithString:@"address/validate"
                         relativeToURL:baseURL];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [request setHTTPMethod:@"GET"];
     url = [NSURL URLWithString:[[url absoluteString] stringByAppendingFormat:@"?address=%@&api_key=%@", address, self.publicKey]];
     [request setURL:url];
 
     [NSURLConnection sendAsynchronousRequest:request
-                                       queue:self.operationQueue
+                                       queue:operationQueue
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
                                NSDictionary *json = nil;
                                NSError *error = nil;
 
                                if (!connectionError){
                                    json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
-                                   if (!json) {
-                                       failure(error);
+                                   if (json) {
+                                       BOOL isValid = [[json valueForKey:@"is_valid"] boolValue];
+                                       
+                                       NSString *didYouMean = nil;
+                                       if (![[json valueForKey:@"did_you_mean"] isKindOfClass:[NSNull class]]) {
+                                           didYouMean = [json valueForKey:@"did_you_mean"];
+                                       }
+
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           success(isValid, didYouMean);
+                                       });
                                        return;
                                    }
-
-                                   BOOL isValid = [[json valueForKey:@"is_valid"] boolValue];
-                                   
-                                   NSString *didYouMean = nil;
-                                   if (![[json valueForKey:@"did_you_mean"] isKindOfClass:[NSNull class]]) {
-                                       didYouMean = [json valueForKey:@"did_you_mean"];
+                               }
+                               
+                               if (self.performsFallbackValidation) {
+                                   // from http://www.regular-expressions.info/email.html
+                                   NSString *pattern = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+                                   NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:pattern
+                                                                                                     options:NSRegularExpressionCaseInsensitive
+                                                                                                       error:&error];
+                                   NSUInteger matches = [regex matchesInString:address options:0 range:NSMakeRange(0, address.length)];
+                                   if (error) {
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           failure(error);
+                                       });
+                                   } else {
+                                       BOOL isValid = matches != 0;
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           success(isValid, nil);
+                                       });
                                    }
-
-                                   success(isValid, didYouMean);
-
                                } else {
-                                   error = connectionError;
-                                   failure(error);
+                                   dispatch_async(dispatch_get_main_queue(), ^{
+                                       failure(connectionError);
+                                   });
                                }
                            }];
 }
