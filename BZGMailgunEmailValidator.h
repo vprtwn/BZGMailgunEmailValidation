ab06d4bf32a1a0cf5850173ef7ef3acee1f8852e
Ben Guo <benzguo@gmail.com>
2013-10-20 16:13:32 -0400

    Update BZGMailgunEmailValidator.h

--- BZGMailgunEmailValidator.h
+++ BZGMailgunEmailValidator.h
@@ -11,16 +11,15 @@
 /**
  * Returns a validator instance initialized with the given public key and operation queue.
  * @param publicKey Your Mailgun public API key.
- * @param operationQueue The operation queue to which blocks are dispatched when the request completes or fails.
  * @return A BZGMailgunEmailValidator instance.
  */
-+ (BZGMailgunEmailValidator *)validatorWithPublicKey:(NSString *)publicKey operationQueue:(NSOperationQueue *)operationQueue;
++ (BZGMailgunEmailValidator *)validatorWithPublicKey:(NSString *)publicKey;
 
 /**
  * Loads a validation request for the given email address. Executes the success block if the request succeeds and the failure block if the request fails.
  * @param address The email address to validate.
- * @param success The block to execute when the request succeeds (required).
- * @param failure The block to execute when the request fails (optional).
+ * @param success The block to execute when the request succeeds (required). This block is executed on the main queue.
+ * @param failure The block to execute when the request fails (optional). This block is executed on the main queue.
  */
 - (void)validateEmailAddress:(NSString *)address
                      success:(void (^)(BOOL isValid, NSString *didYouMean))success
